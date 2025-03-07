b9c8e5061f97a450d75c6c93a965116d4ec341ef
lanroo <yladacs@gmail.com>
2025-03-07 15:28:45 -0300

    fix: atualiza base URL para domínio do Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://www.lardemariapara.org/',
+  base: 'https://lar-de-maria.vercel.app/',
   assetsInclude: [
     "**/*.jpg",
     "**/*.JPG",
